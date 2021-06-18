93305b7ff0489cae5bbce91095ac169752b0f808
PetrovicAleksandra <56756773+PetrovicAleksandra@users.noreply.github.com>
2021-06-18 18:37:08 +0200

    dodate slike

---
+++ src/components/Images.js
@@ -0,0 +1,17 @@
+export const Images = [
+    {
+        url: 'https://blog.alexandar-cosmetics.com/wp-content/uploads/2020/06/sisanje-pasa-u-salonu-768x512.jpg',
+        opis: 'Sredjivanje noktiju vaseg ljubimca',
+        cName: 'img'
+    },
+    {
+        url: 'https://www.cesarsway.com/wp-content/uploads/2019/06/AdobeStock_56951807-1024x681.jpeg',
+        opis: 'Šišanje vašeg ljubimca',
+        cName: 'img'
+    },
+    {
+        url: 'https://doggroomingguide.cabanova.com/files/nljm-4ezdye4jl84d-1iulr.jpg',
+        opis: 'Kupanje vašeg ljubimca',
+        cName: 'img'
+    }
+]
