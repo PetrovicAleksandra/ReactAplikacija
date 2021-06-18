e05f0ef907d821abbeddda5ac97842179da591f1
PetrovicAleksandra <56756773+PetrovicAleksandra@users.noreply.github.com>
2021-06-18 18:33:34 +0200

    App povezan sa funkcijama

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar"
+import Pocetna from './components/pages/Pocetna';
 import './App.css';
-
+import Kontakt from './components/pages/Kontakt';
+import UlogujSe from './components/pages/UlogujSe';
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-    </div>
+    <>
+    <Router>
+    <Navbar />
+    <Switch>
+      <Route path='/' exact component={Pocetna}/>
+      <Route path='/pocetna' exact component={Pocetna}/>
+      <Route exact path='/kontakt' component={Kontakt}/>
+      <Route path='/ulogujse' component={{UlogujSe}.openModal} />
+    </Switch>
+    </Router>
+    </>
   );
 }
-
+setInterval(Pocetna, 10000);
 export default App;
